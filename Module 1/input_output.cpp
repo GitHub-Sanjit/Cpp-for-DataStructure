67f15ed54cef416f3f51db2d5dcc3e62a3285561
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-05-21 13:01:07 +0600

    Cpp output

---
+++ Module 1/input_output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // std::cout<<"Hello World";
+    // std::cout<<"Hello \n";
+    // std::cout<<"World";
+    // std::cout<<"World "<<"Hello";
+    // std::cout << "World " << std::endl<<"Hello";
+    // std::cout<<"World";
+    int a = 100,b=200;
+    // std::cout << a<<std::endl<<b;
+    std::cout << a<<" "<<b;
+    return 0;
+}
